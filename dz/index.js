e783e55afaff3974cd1854c93716fc1805634cb2
Elena Seliutsina <Elena7mal86@mail.ru>
2022-06-14 12:16:33 +0300

    added corection

--- dz/index.js
+++ dz/index.js
@@ -1,48 +1,80 @@
-const agregateUsersAndTodos = async(user) => {
-   const url = 'https://jsonplaceholder.typicode.com/users';
-   const todosUrl = 'https://jsonplaceholder.typicode.com/todos'
-   try {
-     const res = await fetch(url).then(Response => Response.json())
-     const todosRes = await fetch(todosUrl).then(Response => Response.json())
-     // console.log(res,todosRes);
-     const aggregate = (users,todoses) => {
-       
-      const aggregate2 = todoses.reduce ((result, todos) => 
-      {
-      result [todos.userId]= todos;
-        return result;},{});
-        return users.map ((user) => (
-          {...user, 
-         todos:aggregate2[user.id],}));
-        }
+const agregateUsersAndTodos = async (user) => {
+  const usersUrl = "https://jsonplaceholder.typicode.com/users";
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  try {
+    const res = await fetch(usersUrl).then((response) => response.json());
+    const todosRes = await fetch(todosUrl).then((response) => response.json());
+   
+    // console.log(res,todosRes);
+    const aggregate = (users, todoses) => {
+       const aggregateUsersTodos = todoses.reduce((result, todos) => {
+        if (!result[todos.userId]) {
+          result[todos.userId] = [todos];
+      } else {
+          result[todos.userId].push(todos)
+      }
+        return result;
+      }, {});
+      console.log(aggregateUsersTodos);
+      return users.map((user) => ({ ...user, todos: aggregateUsersTodos[user.id] }));
+    };
     console.log(aggregate(res, todosRes));
-  }
-  catch (error) {
-      
-      }}
-      console.log(agregateUsersAndTodos())
+
+  } catch (error) {}
+
+}; 
+console.log(agregateUsersAndTodos());
 
 /* 2*/
-const agregateUsersAndTodos2 = async(user) => {
-  const url2 = 'https://jsonplaceholder.typicode.com/users';
-  const todosUrl2 = 'https://jsonplaceholder.typicode.com/todos'
+const agregateUsersAndTodos2 = async (user) => {
+  const url2 = "https://jsonplaceholder.typicode.com/users";
+  const todosUrl2 = "https://jsonplaceholder.typicode.com/todos";
   try {
-    const res2 = await fetch(url2).then(Response => Response.json())
-    const todosRes2 = await fetch(todosUrl2).then(Response => Response.json())
+    const res2 = await fetch(url2).then((response) => response.json());
+    const todosRes2 = await fetch(todosUrl2).then((Response) =>
+      Response.json()
+    );
 
-    const aggregate = (users,todoses) => {
-     return users.map ((user) => {
-       const aggregateUsers = todoses.filter(todos =>todos.userId === user.id)
+    const aggregate = (users, todoses) => {
+      return users.map((user) => {
+        const aggregateUsers = todoses.filter(
+          (todos) => {return todos.userId === user.id}
+        );
 
-       return {
-         todos: aggregateUsers,
-         ...user,
-       }
-     });
-  }
-  console.log(aggregate(res2, todosRes2));
-  }
-  catch (error) {
-      
-      }}
-      console.log(agregateUsersAndTodos())
+        return {
+          todos: aggregateUsers,
+          ...user,
+        };
+       
+      });
+    };
+    console.log(aggregate(res2, todosRes2));
+  } catch (error) {}
+};
+console.log(agregateUsersAndTodos());
+
+//   /*3*/
+
+//    const agregateUsersAndTodos3 = async(user) => {
+//       const url3 = 'https://jsonplaceholder.typicode.com/users';
+//       const todosUrl3 = 'https://jsonplaceholder.typicode.com/todos'
+//       try {
+//          const res3 = await fetch(url3).then(Response => Response.json())
+//          const todosRes3 = await fetch(todosUrl3).then(Response => Response.json())
+
+//          const aggregate3 = (users,todoses) => {
+
+//          const aggregateUsersMap = new Map();
+
+//          todoses.forEach((todos) => {aggregateUsersMap.set(todos,)
+//          });
+//          return users.map ((user) => (
+//             {...user,
+//             todos:aggregateUsersMap.get(user.id),}))
+//          }
+//       console.log(aggregate3(res3, todosRes3));
+//       }
+//       catch (error) {
+
+//       }}
+//       console.log(agregateUsersAndTodos3())
