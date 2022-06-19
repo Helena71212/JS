778da07f9be3f16b0e43b1559230e8ccd2e51e09
Elena Seliutsina <Elena7mal86@mail.ru>
2022-06-19 12:59:30 +0300

    hw

--- dz/index.js
+++ dz/index.js
@@ -1,22 +1,21 @@
 
 
-const arrPairs = ["red", "green", "red", "red","red","red","red","red","red","red","red",]
+const arrPairs = ["red", "green", "red", "red","red","red","red","red",];
+function countPairGloves (arr) {
+  const sumGloves = arr.reduce((acc, element) => {
+        acc[element] = (acc[element] || 0) + 1;
+        return acc;
+    }, {})
 
-
-const countColorPair =(arr) => {
- const countGloves = {}
- 
- for (let i = 0; i < arr.length; i++) {
-  if (!countGloves[arr[i]]) countGloves[arr[i]] = 1 
-  else countGloves[arr[i]] = countGloves[arr[i]]+1 ;
- }
- let pairs = 0
- for(key in countGloves){
- if (countGloves[key] % 2 === 0) pairs += Math.floor (countGloves[key] / 2)
- }
-return pairs;
+    let result = 0;
+  Object.keys(sumGloves).forEach(
+    (el) => (result += Math.floor(sumGloves[el] / 2))
+  );
+  return result; 
 }
 
-console.log(countColorPair(arrPairs));
+console.log(countPairGloves(arrPairs));
+
+
 
 
